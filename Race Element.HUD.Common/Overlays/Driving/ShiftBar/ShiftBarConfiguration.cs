6b59b643105ca6d10aac22450252fc1821f9b864
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-11-01 00:13:15 +0100

    shiftbar now automatically re-adjust the max rpm

--- Race Element.HUD.Common/Overlays/Driving/ShiftBar/ShiftBarConfiguration.cs
+++ Race Element.HUD.Common/Overlays/Driving/ShiftBar/ShiftBarConfiguration.cs
@@ -1,4 +1,6 @@
-﻿using RaceElement.HUD.Overlay.Configuration;
+﻿using RaceElement.Data.Games;
+using RaceElement.HUD.Overlay.Configuration;
+using System.Drawing;
 
 namespace RaceElement.HUD.Common.Overlays.Driving.ShiftBar;
 internal sealed class ShiftBarConfiguration : OverlayConfiguration
@@ -20,7 +22,7 @@ internal sealed class ShiftBarConfiguration : OverlayConfiguration
     public RenderGrouping Render { get; init; } = new();
     public sealed class RenderGrouping
     {
-        [IntRange(50, 120, 10)]
+        [IntRange(50, 120, 10, GameMaxs = [80], MaxGames = [Game.iRacing])]
         public int RefreshRate { get; init; } = 80;
     }
 
@@ -31,6 +33,44 @@ internal sealed class ShiftBarConfiguration : OverlayConfiguration
         [ToolTip("Hide Rpms in the bar, starting from 0.\nIt will always leave 2000 RPM.")]
         [IntRange(0, 9000, 100)]
         public int HideRpm { get; init; } = 3000;
+    }
+
+
+    [ConfigGrouping("Upshift Percentages", "Adjust the Early and Upshift percentages.\n" +
+        "The displayed early and upshift RPM texts only show in the GUI.\n" +
+        "These RPMs will update if you are currently in a lobby, once you adjust any of the settings.")]
+    [HideForGame(Game.RaceRoom)]
+    public UpshiftGrouping Upshift { get; init; } = new UpshiftGrouping();
+    public sealed class UpshiftGrouping
+    {
+        [ToolTip("Sets the percentage of max rpm required to activate the early upshift color")]
+        [FloatRange(80.0f, 96.8f, 0.02f, 2)]
+        public float Early { get; init; } = 94.0f;
+
+        [ToolTip("Sets the percentage of max rpm required to activate the upshift color")]
+        [FloatRange(97f, 99.98f, 0.02f, 2)]
+        public float Upshift { get; init; } = 97.3f;
+    }
+
+    [ConfigGrouping("Colors", "Adjust the colors used in the shift bar")]
+    public ColorsGrouping Colors { get; init; } = new ColorsGrouping();
+    public sealed class ColorsGrouping
+    {
+        public Color NormalColor { get; init; } = Color.FromArgb(255, 5, 255, 5);
+        [IntRange(75, 255, 1)]
+        public int NormalOpacity { get; init; } = 255;
+
+        public Color EarlyColor { get; init; } = Color.FromArgb(255, 255, 255, 0);
+        [IntRange(75, 255, 1)]
+        public int EarlyOpacity { get; init; } = 255;
+
+        public Color UpshiftColor { get; init; } = Color.FromArgb(255, 255, 4, 4);
+        [IntRange(75, 255, 1)]
+        public int UpshiftOpacity { get; init; } = 255;
+
+        public Color FlashColor { get; init; } = Color.FromArgb(255, 0, 131, 255);
+        [IntRange(75, 255, 1)]
+        public int FlashOpacity { get; init; } = 255;
     }
 }
 
--- Race Element.HUD.Common/Overlays/Driving/ShiftBar/ShiftBarOverlay.cs
+++ Race Element.HUD.Common/Overlays/Driving/ShiftBar/ShiftBarOverlay.cs
@@ -1,15 +1,13 @@
-﻿using RaceElement.Data.Common;
-using RaceElement.HUD.Overlay.Configuration;
+﻿using RaceElement.Core.Jobs.Loop;
+using RaceElement.Data.Common;
+using RaceElement.Data.Games;
 using RaceElement.HUD.Overlay.Internal;
 using RaceElement.HUD.Overlay.OverlayUtil;
 using RaceElement.Util.SystemExtensions;
-using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace RaceElement.HUD.Common.Overlays.Driving.ShiftBar;
 
@@ -23,15 +21,20 @@ internal sealed class ShiftBarOverlay : CommonAbstractOverlay
 {
     private readonly ShiftBarConfiguration _config = new();
 
-    private CachedBitmap _cachedbackground;
-    private CachedBitmap _cachedBar;
+    private CachedBitmap _cachedBackground;
     private CachedBitmap _cachedRpmLines;
 
-    private RectangleF WorkingSpace;
+    private readonly List<CachedBitmap> _cachedColorBars = [];
+    private readonly List<(float percentage, Color color)> _colors = [];
+
+    private readonly Rectangle WorkingSpace;
     private RectangleF BarSpace;
 
     private record struct ShiftBarDataModel(int Rpm, int MaxRpm);
     private ShiftBarDataModel _model;
+
+    private MaxRpmDetectionJob _maxRpmDetectionJob;
+
 
     public ShiftBarOverlay(Rectangle rectangle) : base(rectangle, "Shift Bar")
     {
@@ -41,46 +44,73 @@ internal sealed class ShiftBarOverlay : CommonAbstractOverlay
         RefreshRateHz = _config.Render.RefreshRate;
     }
 
-    public override void SetupPreviewData() => _model = new(8234, 9250);
+    public override void SetupPreviewData() => _model = new(Random.Shared.Next(8000, 9200), 9250);
 
+    private void UpdateColorDictionary()
+    {
+        // configured percentages (0-100%)
+        float earlyPercentage = _config.Upshift.Early;
+        float upshiftPercentage = _config.Upshift.Upshift;
+
+        if (GameWhenStarted.HasFlag(Game.RaceRoom))
+        {
+            float maxRpm = SimDataProvider.LocalCar.Engine.MaxRpm;
+            float upshiftRpm = SimDataProvider.LocalCar.Engine.ShiftUpRpm;
+            if (maxRpm > 0 && upshiftRpm > 0)
+            {
+                upshiftPercentage = upshiftRpm * 100 / maxRpm;
+                earlyPercentage = upshiftPercentage * 0.96f;
+            }
+        }
+
+        _colors.Clear();
+        _colors.Add((0.7f, Color.FromArgb(_config.Colors.NormalOpacity, _config.Colors.NormalColor)));
+        _colors.Add((earlyPercentage / 100f, Color.FromArgb(_config.Colors.EarlyOpacity, _config.Colors.EarlyColor)));
+        _colors.Add((upshiftPercentage / 100f, Color.FromArgb(_config.Colors.UpshiftOpacity, _config.Colors.UpshiftColor)));
+        _colors.Add((upshiftPercentage / 100f, Color.FromArgb(_config.Colors.UpshiftOpacity, _config.Colors.UpshiftColor)));
+    }
     public override void BeforeStart()
     {
-        int scaledWorkingWidth = (int)(Math.Ceiling(WorkingSpace.Width * Scale));
-        int scaledWorkingHeight = (int)(Math.Ceiling(WorkingSpace.Height * Scale));
+        UpdateColorDictionary();
 
-        RectangleF scaledWorkingSize = new(0, 0, scaledWorkingWidth, scaledWorkingHeight);
+        int horizontalBarPadding = 2;
+        int verticalBarPadding = 2;
+        BarSpace = new(horizontalBarPadding, verticalBarPadding, WorkingSpace.Width - horizontalBarPadding * 2, WorkingSpace.Height - verticalBarPadding * 2);
 
-        int horizontalPadding = 2;
-        int verticalPadding = 2;
-        BarSpace = new(horizontalPadding, verticalPadding, scaledWorkingWidth - horizontalPadding * 2, scaledWorkingHeight - verticalPadding * 2);
-
-        _cachedbackground = new(scaledWorkingWidth, scaledWorkingHeight, g =>
+        _cachedBackground = new(WorkingSpace.Width, WorkingSpace.Height, g =>
         {
             int horizontalPadding = 1;
             int verticalPadding = 1;
-            RectangleF barArea = new(horizontalPadding, verticalPadding, scaledWorkingWidth - horizontalPadding * 2, scaledWorkingHeight - verticalPadding * 2);
+            RectangleF barArea = new(horizontalPadding, verticalPadding, WorkingSpace.Width - horizontalPadding * 2, WorkingSpace.Height - verticalPadding * 2);
 
             g.CompositingQuality = CompositingQuality.HighQuality;
             g.SmoothingMode = SmoothingMode.AntiAlias;
             using SolidBrush darkBrush = new(Color.FromArgb(90, Color.Black));
             g.FillRoundedRectangle(darkBrush, Rectangle.Round(barArea), 4);
-
         });
 
-        _cachedBar = new(scaledWorkingWidth, scaledWorkingHeight, g =>
-        {
-            Rectangle area = Rectangle.Round(WorkingSpace);
-            using LinearGradientBrush blackToGreenGradient = new(area, Color.FromArgb(170, 0, 170, 0), Color.FromArgb(170, 0, 255, 0), 0f);
-            g.FillRoundedRectangle(blackToGreenGradient, Rectangle.Round(BarSpace), 4);
+        _cachedColorBars.Clear();
+        for (int i = 0; i < _colors.Count; i++)
+            _cachedColorBars.Add(new(WorkingSpace.Width, WorkingSpace.Height, g =>
+            {
+                Rectangle area = Rectangle.Round(WorkingSpace);
 
-            using LinearGradientBrush whiteToDarkGradientVertical = new(area, Color.FromArgb(0, 0, 0, 0), Color.FromArgb(60, 20, 20, 20), -90f);
-            g.FillRoundedRectangle(whiteToDarkGradientVertical, Rectangle.Round(BarSpace), 4);
+                using LinearGradientBrush whiteToDarkGradientVertical = new(area, Color.FromArgb(0, 0, 0, 0), Color.FromArgb(60, 20, 20, 20), -90f);
+                g.FillRoundedRectangle(whiteToDarkGradientVertical, Rectangle.Round(BarSpace), 4);
 
-            using HatchBrush hatchBrush = new(HatchStyle.LightUpwardDiagonal, Color.FromArgb(95, 0, 20, 0), Color.Transparent);
-            g.FillRoundedRectangle(hatchBrush, Rectangle.Round(BarSpace), 4);
-        });
 
-        _cachedRpmLines = new(scaledWorkingWidth, scaledWorkingHeight, g =>
+                Color primaryColor = _colors.ElementAt(i).Item2;
+                Color secondaryColor = Color.FromArgb(170, primaryColor);
+
+                using LinearGradientBrush blackToGreenGradient = new(area, secondaryColor, primaryColor, 0f);
+                g.FillRoundedRectangle(blackToGreenGradient, Rectangle.Round(BarSpace), 4);
+
+                Color hatchColor = Color.FromArgb(95, primaryColor);
+                using HatchBrush hatchBrush = new(HatchStyle.LightUpwardDiagonal, hatchColor/* Color.FromArgb(95, 0, 20, 0)*/, Color.Transparent);
+                g.FillRoundedRectangle(hatchBrush, Rectangle.Round(BarSpace), 4);
+            }));
+
+        _cachedRpmLines = new(WorkingSpace.Width, WorkingSpace.Height, g =>
         {
             int lineCount = (int)Math.Floor((_model.MaxRpm - _config.Data.HideRpm) / 1000d);
 
@@ -90,7 +120,7 @@ internal sealed class ShiftBarOverlay : CommonAbstractOverlay
 
             lineCount.ClipMin(0);
             using SolidBrush brush = new(Color.FromArgb(220, Color.Black));
-            using Pen linePen = new(brush, 1.5f * Scale);
+            using Pen linePen = new(brush, 1.6f * Scale);
 
             double thousandPercent = 1000d / (_model.MaxRpm - _config.Data.HideRpm) * lineCount;
             double baseX = _config.Size.Width * Scale / lineCount * thousandPercent;
@@ -100,36 +130,82 @@ internal sealed class ShiftBarOverlay : CommonAbstractOverlay
                 g.DrawLine(linePen, x, 1, x, _config.Size.Height * Scale - 1);
             }
         });
+
+        if (!IsPreviewing)
+        {
+            _maxRpmDetectionJob = new(this) { IntervalMillis = 1000 };
+            _maxRpmDetectionJob.Run();
+        }
+    }
+
+    public override void BeforeStop()
+    {
+        _maxRpmDetectionJob?.CancelJoin();
+        _cachedBackground?.Dispose();
+        _cachedRpmLines?.Dispose();
+        foreach (var item in _cachedColorBars)
+            item.Dispose();
+        _cachedColorBars.Clear();
     }
     public override bool ShouldRender() => true;
 
     public override void Render(Graphics g)
     {
-        int workingSpaceWidth = (int)WorkingSpace.Width;
-        int workingSpaceHeight = (int)WorkingSpace.Height;
-
-        _cachedbackground.Draw(g, 0, 0, workingSpaceWidth, workingSpaceHeight);
+        _cachedBackground?.Draw(g, 0, 0, WorkingSpace.Width, WorkingSpace.Height);
 
         if (!IsPreviewing)
         {
             _model.Rpm = SimDataProvider.LocalCar.Engine.Rpm;
             _model.MaxRpm = SimDataProvider.LocalCar.Engine.MaxRpm;
 
-            _model.Rpm = Random.Shared.Next(100, 9500);
+            // Test data, remove for release.
+            _model.Rpm = Random.Shared.Next(5000, 9500);
             _model.MaxRpm = 9500;
         }
 
-        RectangleF percented = new(BarSpace.ToVector4());
-        double percent = 0;
-        if (_model.Rpm > 0 && _model.MaxRpm > 0)
-            percent = (double)_model.Rpm / _model.MaxRpm;
-        percent.Clip(0, 1);
-        percented.Width = (float)(BarSpace.Width * percent);
-        g.SetClip(percented);
+        DrawBar(g);
 
-        _cachedBar.Draw(g, 0, 0, workingSpaceWidth, workingSpaceHeight);
-
-        _cachedRpmLines.Draw(g, 0, 0, workingSpaceWidth, workingSpaceHeight);
+        _cachedRpmLines.Draw(g, 0, 0, WorkingSpace.Width, WorkingSpace.Height);
     }
 
+    private void DrawBar(Graphics g)
+    {
+        RectangleF percented = new(BarSpace.ToVector4());
+        double rpmPercentage = 0;
+        if (_model.Rpm > 0 && _model.MaxRpm > 0)
+            rpmPercentage = (double)_model.Rpm / _model.MaxRpm;
+
+        rpmPercentage.Clip(0, 1);
+        percented.Width = (float)(BarSpace.Width * rpmPercentage);
+
+        int barIndex = GetCurrentColorBarIndex(rpmPercentage);
+        g.SetClip(percented);
+        _cachedColorBars[barIndex].Draw(g, 0, 0, WorkingSpace.Width, WorkingSpace.Height);
+        g.ResetClip();
+    }
+
+    private int GetCurrentColorBarIndex(double percentage)
+    {
+        int barIndex = 0;
+        var colorSpan = CollectionsMarshal.AsSpan(_colors);
+        for (int i = 0; i < colorSpan.Length; i++)
+        {
+            if (percentage < colorSpan[i].percentage) break;
+            barIndex = i;
+        }
+        return barIndex;
+    }
+
+    private class MaxRpmDetectionJob(ShiftBarOverlay shiftBarOverlay) : AbstractLoopJob
+    {
+        public override void RunAction()
+        {
+            var model = shiftBarOverlay._model;
+            if (SimDataProvider.LocalCar.Engine.MaxRpm != model.MaxRpm)
+            {
+                shiftBarOverlay?._cachedRpmLines.Render();
+                shiftBarOverlay?.UpdateColorDictionary();
+            }
+        }
+    }
 }
