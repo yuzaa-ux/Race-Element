0deb3bf25f38074f7d70ea683d12d36fdf3031fd
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-11-01 03:44:59 +0100

    Create TimerDebugOverlay.cs

---
+++ Race Element.HUD.Common/Overlays/Pitwall/TimerDebug/TimerDebugOverlay.cs
@@ -0,0 +1,31 @@
+﻿using RaceElement.HUD.Overlay.Internal;
+using RaceElement.HUD.Overlay.Util;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceElement.HUD.Common.Overlays.Pitwall.TimerDebug;
+[Overlay(Name = "Timer Debug", Description = "Debug timers", OverlayType = OverlayType.Pitwall)]
+internal sealed class TimerDebugOverlay : CommonAbstractOverlay
+{
+    InfoPanel _panel;
+    public TimerDebugOverlay(Rectangle rectangle) : base(rectangle, "Timer Debug")
+    {
+        _panel = new(11, 300);
+    }
+
+    public override bool ShouldRender() => true;
+
+    public override void Render(Graphics g)
+    {
+        _panel.AddLine("TP Hz", $"{TimeProvider.System.TimestampFrequency}");
+
+        _panel.AddLine("HR?", $"{Stopwatch.IsHighResolution}");
+        _panel.AddLine("SW Hz", $"{Stopwatch.Frequency}");
+        _panel.Draw(g);
+    }
+}
