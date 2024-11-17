9f4657769abc51a8ac68f1f789e9dfe3360226b0
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-11-17 21:20:00 +0100

    add more options for the shift rpm hud

---
+++ Race Element.HUD.Common/Overlays/Driving/ShiftRpm/ShiftRpmConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using RaceElement.HUD.Overlay.Configuration;
+
+namespace RaceElement.HUD.Common.Overlays.Driving.ShiftRpm;
+internal sealed class ShiftRpmConfiguration : OverlayConfiguration
+{
+    public ShiftRpmConfiguration() => GenericConfiguration.AllowRescale = false;
+
+    [ConfigGrouping("General", "General options")]
+    public GeneralGrouping General { get; init; } = new();
+    public sealed class GeneralGrouping
+    {
+        [ToolTip("Size of the font")]
+        [FloatRange(12.0f, 90.0f, 0.5f, 1)]
+        public float FontSize { get; init; } = 12.0f;
+
+        [ToolTip("Change the Font")]
+        public RpmTextFont Font { get; init; } = RpmTextFont.Conthrax;
+
+        [IntRange(0, 30, 1)]
+        public int ExtraDigitSpacing { get; init; } = 0;
+    }
+
+    public enum RpmTextFont { Roboto, Conthrax, Obitron }
+}
--- Race Element.HUD.Common/Overlays/Driving/ShiftRpm/ShiftRpmOverlay.cs
+++ Race Element.HUD.Common/Overlays/Driving/ShiftRpm/ShiftRpmOverlay.cs
@@ -1,8 +1,11 @@
-﻿using RaceElement.HUD.Overlay.Internal;
+﻿using RaceElement.HUD.Overlay.Configuration;
+using RaceElement.HUD.Overlay.Internal;
+using RaceElement.HUD.Overlay.OverlayUtil;
+using RaceElement.HUD.Overlay.Util;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,22 +16,99 @@ Name = "Shift RPM",
 Description = "The current engine RPM as text")]
 internal sealed class ShiftRpmOverlay : CommonAbstractOverlay
 {
+    private readonly ShiftRpmConfiguration _config = new();
 
+    private CachedBitmap _cachedBackground;
+
+    private RpmBitmaps _bitmaps;
     public ShiftRpmOverlay(Rectangle rectangle) : base(rectangle, "Shift RPM")
     {
+        RefreshRateHz = 20;
+    }
+
+    public override void BeforeStart()
+    {
+        _bitmaps = new RpmBitmaps(_config.General.Font, _config.General.FontSize);
+        Width = 5 * _bitmaps.Dimension.Width + _config.General.ExtraDigitSpacing * 4;
+        Height = _bitmaps.Dimension.Height;
+
+        _cachedBackground = new(Width, Height, g =>
+        {
+            RectangleF barArea = new(0, 0, Width - 1, Height - 1);
+
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            using SolidBrush darkBrush = new(Color.FromArgb(90, Color.Black));
+            g.FillRoundedRectangle(darkBrush, Rectangle.Round(barArea), 3);
+            using Pen darkPen = new(darkBrush, 1);
+            g.DrawRoundedRectangle(darkPen, Rectangle.Round(barArea), 3);
+        });
+    }
+
+    public override void BeforeStop()
+    {
+        _cachedBackground?.Dispose();
+        _bitmaps?.Dispose();
     }
 
     public override void Render(Graphics g)
     {
+        _cachedBackground?.Draw(g);
 
+        int x = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            _bitmaps.GetForNumber((byte)Random.Shared.Next(0, 9)).Draw(g, new(x, 0));
+            x += _bitmaps.Dimension.Width + _config.General.ExtraDigitSpacing;
+        }
     }
 }
-internal class RpmBitmaps
+
+internal sealed class RpmBitmaps : IDisposable
 {
-    public CachedBitmap? GetForNumber(byte number)
+    private readonly CachedBitmap[] _rpmBitmaps = new CachedBitmap[10];
+    public readonly (int Width, int Height) Dimension;
+    public RpmBitmaps(ShiftRpmConfiguration.RpmTextFont font, float fontSize)
+    {
+        GenerateBitMaps(font, fontSize);
+        Dimension = (_rpmBitmaps[0].Width, _rpmBitmaps[0].Height);
+    }
+
+    private void GenerateBitMaps(ShiftRpmConfiguration.RpmTextFont fontType, float fontSize)
+    {
+        Font font = fontType switch
+        {
+            ShiftRpmConfiguration.RpmTextFont.Conthrax => FontUtil.FontConthrax(fontSize),
+            ShiftRpmConfiguration.RpmTextFont.Obitron => FontUtil.FontOrbitron(fontSize),
+            ShiftRpmConfiguration.RpmTextFont.Roboto => FontUtil.FontRoboto(fontSize),
+            _ => FontUtil.FontConthrax(fontSize),
+        };
+
+        StringFormat format = StringFormat.GenericDefault;
+        format.Alignment = StringAlignment.Center;
+        format.LineAlignment = StringAlignment.Center;
+        format.FormatFlags = StringFormatFlags.NoClip;
+
+        int bitmapWidth = (int)(fontSize + 4);
+        int bitmapHeight = bitmapWidth + 4;
+        for (int i = 0; i <= 9; i++)
+        {
+            _rpmBitmaps[i] = new(bitmapWidth, bitmapHeight, g =>
+            {
+                g.DrawStringWithShadow($"{i}", font, Color.White, new RectangleF(0, 2, bitmapWidth, bitmapHeight - 2), format);
+            });
+        }
+    }
+    public CachedBitmap GetForNumber(byte number)
     {
         ArgumentOutOfRangeException.ThrowIfGreaterThan(number, 9);
+        return _rpmBitmaps.AsSpan()[number];
+    }
 
-        return null;
+    public void Dispose()
+    {
+        foreach (CachedBitmap cachedBitmap in _rpmBitmaps)
+            cachedBitmap?.Dispose();
     }
 }
+
