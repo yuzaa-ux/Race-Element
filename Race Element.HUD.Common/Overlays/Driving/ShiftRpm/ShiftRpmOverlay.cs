c689992ba0b8770a90aa0faca667ab7b32000f6f
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-11-17 19:38:37 +0100

    Create ShiftRpmOverlay.cs

---
+++ Race Element.HUD.Common/Overlays/Driving/ShiftRpm/ShiftRpmOverlay.cs
@@ -0,0 +1,34 @@
+﻿using RaceElement.HUD.Overlay.Internal;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceElement.HUD.Common.Overlays.Driving.ShiftRpm;
+[Overlay(
+Name = "Shift RPM",
+Description = "The current engine RPM as text")]
+internal sealed class ShiftRpmOverlay : CommonAbstractOverlay
+{
+
+    public ShiftRpmOverlay(Rectangle rectangle) : base(rectangle, "Shift RPM")
+    {
+    }
+
+    public override void Render(Graphics g)
+    {
+
+    }
+}
+internal class RpmBitmaps
+{
+    public CachedBitmap? GetForNumber(byte number)
+    {
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(number, 9);
+
+        return null;
+    }
+}
