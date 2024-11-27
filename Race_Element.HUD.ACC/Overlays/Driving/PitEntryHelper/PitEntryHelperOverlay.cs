f22b1256a769540a36cdd409b538da80d9a5c925
Reinier Klarenberg <reinierklarenberg@gmail.com>
2024-11-27 17:48:42 +0100

    Update PitEntryHelperOverlay.cs

--- Race_Element.HUD.ACC/Overlays/Driving/PitEntryHelper/PitEntryHelperOverlay.cs
+++ Race_Element.HUD.ACC/Overlays/Driving/PitEntryHelper/PitEntryHelperOverlay.cs
@@ -7,11 +7,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace RaceElement.HUD.ACC.Overlays.Driving.PitEntryHelper;
+#if DEBUG
 [Overlay(
     Name = "Pit Entry Helper",
     Description = "Provides an accurate entry point of the pits and the required speed to match using a bar.\n Helps you to brake as late as possible for pit entry.",
     Authors = ["Reinier Klarenberg"]
 )]
+#endif
 internal class PitEntryHelperOverlay : AbstractOverlay
 {
     public PitEntryHelperOverlay(Rectangle rectangle) : base(rectangle, "Pit Entry Helper")
